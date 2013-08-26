6b9d2d10004f77c62572e4d64813806f98d310cd
Steffen Müthing <muething@dune-project.org>
2013-08-26 20:52:55 +0200

    [CMake] Add CMake macros

---
+++ cmake/modules/DuneTypeTreeMacros.cmake
@@ -0,0 +1,4 @@
+find_package(CXXInitializerList)
+find_package(CXXTemplateAliases)
+find_package(CXXDecltype)
+find_package(GCCTypeof)
---
+++ cmake/modules/FindCXXTemplateAliases.cmake
@@ -0,0 +1,34 @@
+# Module that checks whether the compiler supports
+# C++11 template aliases.
+#
+# Sets the following variable:
+# HAVE_TEMPLATE_ALIASES
+#
+# perform tests
+include(CheckCXXSourceCompiles)
+
+check_cxx_source_compiles("
+  template<typename T, typename U>
+  struct A
+  {};
+
+  template<typename T>
+  using A1 = A<T,int>;
+
+  template<typename U>
+  using A2 = A<int,U>;
+
+  template<typename T, typename U>
+  struct assert_equal;
+
+  template<typename T>
+  struct assert_equal<T,T>
+  {};
+
+  int main()
+  {
+    assert_equal<A1<int>,A2<int> >();
+    assert_equal<A<bool,int>,A1<bool> >();
+    return 0;
+  }"
+  HAVE_TEMPLATE_ALIASES)
