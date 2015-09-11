5346da63491fd288171cdb19ae6e8e4fec07cb2b
Steffen Müthing <muething@dune-project.org>
2015-09-11 19:32:53 +0200

    [child extraction] Major overhaul of child extraction
    This patch adds a new, completely rewritten child extraction mechanism
    using all of the shiny features offered by C++11. It has one dynamic
    and two static entry points:
    
    * If you want to access a child of a tree node, call the method
      Dune;:TypeTree::child(node,<subtree information>). This method can be
      called with either
    
      * a combination of run time indices (given as simple variables of type
        std::size_t) and compile time indices using index_constant,
    
      * a TreePath object,
    
      * or a HybridTreePath object.
    
    * If you want access to the type of a child node in a tree, you have to
      use one of two separate entry points depending on whether you want to
      supply the path as a list of indices or the type of a TreePath /
      HybridTreePath object, as there is no overload support for templates.
    
      * Dune::TypeTree::Child<Node,std::size_t...> is a template aliases
        that can be used for a list of known index values (or a template
        pack of them).
    
      * Dune::TypeTree::ChildForTreePath<Node,TreePath> accepts the type of
        either a TreePath or a HybridTreePath to describe the path.
    
    Right now, the new mechanism doesn't deal with accessing the storage of
    child nodes; before doing that, we'll have to think about storage
    strategies in the future (in particular std::shared_ptr vs. std::unique_ptr).

--- dune/typetree/childextraction.hh
+++ dune/typetree/childextraction.hh
@@ -21,6 +21,10 @@ namespace Dune {
     //! \{
 
     //! Extract the type of the child of Node at position TreePath.
+    /**
+     * \deprecated extract_child_type is deprecated and will be removed after TypeTree 2.4,
+     *             use the template alias Child instead.
+     */
     template<typename Node, typename TreePath>
     struct extract_child_type
     {
@@ -329,7 +333,291 @@ namespace Dune {
     }
 
 
+    namespace impl {
+
+      // ********************************************************************************
+      // end of the recursion, there are no child indices, so just return the node itself
+      // ********************************************************************************
+
+      template<typename Node>
+      auto child(Node&& node) -> decltype(std::forward<Node>(node))
+      {
+        return std::forward<Node>(node);
+      }
+
+      // ********************************************************************************
+      // next index is a compile-time constant
+      // ********************************************************************************
+
+      // we need a little helper trait to make sure that the node has a templated child()
+      // method
+      template<typename Node, typename _ = decltype(std::declval<Node>().template child<0>())>
+      static constexpr auto _has_template_child_method(Node*) -> std::true_type;
+
+      template<typename Node>
+      static constexpr auto _has_template_child_method(void*) -> std::false_type;
+
+      // This struct lazily evaluates the return type by recursively calling child. This has
+      // to happen lazily because we only want to do it if the child access at the current
+      // level succeeds; otherwise, we would swamp the user with error messages as the algorithm
+      // walks down the remaining indices
+      //
+      // This struct gets created inside an enable_if, but the nested alias template that triggers
+      // the recursion is only instantiated if the enable_if was successful.
+      template<typename Node>
+      struct _lazy_static_decltype
+      {
+        template<typename I, typename... J>
+        using evaluate = decltype(child(std::declval<Node>().template child<I::value>(),std::declval<J>()...));
+      };
+
+      // The actual implementation is rather simple, we just use an overload that requires the first index
+      // to be an index_constant, get the child and then recurse.
+      // It only gets ugly due to the enable_if, but without that trick, the error messages for the user
+      // can get *very* obscure (they are bad enough as it is, concepts where are you?).
+      template<typename Node, std::size_t i, typename... J>
+      auto child(Node&& node, index_constant<i>, J... j) ->
+        typename std::enable_if<
+          decltype(_has_template_child_method(std::declval<typename std::remove_reference<Node>::type*>()))::value &&
+          (i < std::decay<Node>::type::CHILDREN),
+          _lazy_static_decltype<
+            typename std::remove_reference<Node>::type
+            >
+          >::type::template evaluate<index_constant<i>,J...>
+      {
+        return child(std::forward<Node>(node).template child<i>(),j...);
+      }
+
+      // ********************************************************************************
+      // next index is a run-time value
+      // ********************************************************************************
+
+
+      // again, a lazy struct for the recursion to further child nodes (see above for further explanation)
+      template<typename Node>
+      struct _lazy_dynamic_decltype
+      {
+        template<typename... J>
+        using evaluate = decltype(child(std::declval<Node>().child(0),std::declval<J>()...));
+      };
+
+      // The actual implemention here overloads on std::size_t. It is a little less ugly because it currently
+      // has a hard requirement on the PowerNode Tag (although only using is_convertible, as tags can be
+      // inherited (important!).
+      template<typename Node, typename... J>
+      auto child(Node&& node, std::size_t i, J... j) ->
+        typename std::enable_if<
+          std::is_convertible<
+            typename std::remove_reference<Node>::type::NodeTag,
+            PowerNodeTag
+            >::value,
+          _lazy_dynamic_decltype<
+            typename std::remove_reference<Node>::type
+            >
+          >::type::template evaluate<J...>
+      {
+        return child(std::forward<Node>(node).template child(i),j...);
+      }
+
+      template<typename Node, typename... Indices, std::size_t... i>
+      auto child(Node&& node, HybridTreePath<Indices...> tp, Std::index_sequence<i...>) -> decltype(child(std::forward<Node>(node),treePathEntry<i>(tp)...))
+      {
+        return child(std::forward<Node>(node),treePathEntry<i>(tp)...);
+      }
+
+
+    } // namespace imp
+
 #endif // DOXYGEN
+
+    //! Extracts the child of a node given by a sequence of compile-time and run-time indices.
+    /**
+     * Use this function to extract a (possibly indirect) child of a TypeTree node.
+     *
+     * Example:
+     *
+     * \code{.cc}
+     * using namespace Dune::TypeTree::Indices; // for compile-time indices
+     * auto&& c = child(node,_4,2,_0,1);
+     * \endcode
+     *
+     * returns the second child of the first child of the third child
+     * of the fifth child of node, where some child lookups were done using
+     * a compile-time index and some using a run-time index.
+     *
+     * \param node        The node from which to extract the child.
+     * \param indices...  A list of indices that describes the path into the tree to the
+     *                    wanted child. These parameters can be a combination of run time indices
+     *                    (for tree nodes that allow accessing their children using run time information,
+     *                    like PowerNode) and instances of index_constant, which work for all types of inner
+     *                    nodes.
+     * \return            A reference to the child, its cv-qualification depends on the passed-in node.
+     */
+    template<typename Node, typename... Indices>
+#ifdef DOXYGEN
+    ImplementationDefined child(Node&& node, Indices... indices)
+#else
+    auto child(Node&& node, Indices... indices) -> decltype(impl::child(std::forward<Node>(node),indices...))
+#endif
+    {
+      return impl::child(std::forward<Node>(node),indices...);
+    }
+
+
+    //! Extracts the child of a node given by a static TreePath object.
+    /**
+     * Use this function to extract a (possibly indirect) child of a TypeTree node.
+     *
+     * Example:
+     *
+     * \code{.cc}
+     * Dune::TypeTree::TreePath<4,2,0,1> tp;
+     * auto&& c = child(node,tp);
+     * \endcode
+     *
+     * returns the second child of the first child of the third child
+     * of the fifth child of node.
+     *
+     * \param node        The node from which to extract the child.
+     * \param treePath    A TreePath object that describes the path into the tree to the
+     *                    wanted child.
+     * \return            A reference to the child, its cv-qualification depends on the passed-in node.
+     */
+    template<typename Node, std::size_t... Indices>
+#ifdef DOXYGEN
+    ImplementationDefined child(Node&& node, TreePath<Indices...> treePath)
+#else
+    auto child(Node&& node, TreePath<Indices...>) -> decltype(child(std::forward<Node>(node),index_constant<Indices>()...))
+#endif
+    {
+      return child(std::forward<Node>(node),index_constant<Indices>{}...);
+    }
+
+
+    //! Extracts the child of a node given by a HybridTreePath object.
+    /**
+     * Use this function to extract a (possibly indirect) child of a TypeTree node.
+     *
+     * Example:
+     *
+     * \code{.cc}
+     * using namespace Dune::TypeTree::Indices; // for compile-time indices
+     * auto tp = Dune::TypeTree::hybridTreePath(_4,2,_0,1);
+     * auto&& c = child(node,tp);
+     * \endcode
+     *
+     * returns the second child of the first child of the third child
+     * of the fifth child of node, where some child lookups were done using
+     * a compile-time index and some using a run-time index.
+     *
+     * \param node        The node from which to extract the child.
+     * \param tree{ath    A HybridTreePath that describes the path into the tree to the
+     *                    wanted child. This tree path object  can be a combination of run time indices
+     *                    (for tree nodes that allow accessing their children using run time information,
+     *                    like PowerNode) and instances of index_constant, which work for all types of inner
+     *                    nodes.
+     * \return            A reference to the child, its cv-qualification depends on the passed-in node.
+     */
+    template<typename Node, typename... Indices>
+#ifdef DOXYGEN
+    ImplementationDefined child(Node&& node, HybridTreePath<Indices...> treePath)
+#else
+    auto child(Node&& node, HybridTreePath<Indices...> tp) -> decltype(impl::child(std::forward<Node>(node),tp,tp.enumerate()))
+#endif
+    {
+      return impl::child(std::forward<Node>(node),tp,Std::index_sequence_for<Indices...>{});
+    }
+
+#ifndef DOXYGEN
+
+    namespace impl {
+
+      template<typename Node, std::size_t... indices>
+      struct _Child
+      {
+        using type = typename std::decay<decltype(child(std::declval<Node>(),index_constant<indices>{}...))>::type;
+      };
+
+    }
+
+#endif // DOXYGEN
+
+    //! Template alias for the type of a child node given by a list of child indices.
+    /**
+     * This template alias is implemented in terms of the free-standing child() functions and uses those
+     * in combination with decltype() to extract the child type.
+
+     * \tparam Node     The type of the parent node.
+     * \tparam indices  A list of index values the describes the path to the wanted child.
+     */
+    template<typename Node, std::size_t... indices>
+    using Child = typename impl::_Child<Node,indices...>::type;
+
+
+#ifndef DOXYGEN
+
+    namespace impl {
+
+      template<typename Node, typename TreePath>
+      struct _ChildForTreePath
+      {
+        using type = typename std::decay<decltype(child(std::declval<Node>(),std::declval<TreePath>()))>::type;
+      };
+
+    }
+
+#endif // DOXYGEN
+
+    //! Template alias for the type of a child node given by a TreePath or a HybridTreePath type.
+    /**
+     * This template alias is implemented in terms of the free-standing child() functions and uses those
+     * in combination with decltype() to extract the child type. It supports both TreePath and
+     * HybridTreePath.
+     *
+     * \tparam Node      The type of the parent node.
+     * \tparam TreePath  The type of a TreePath or a HybridTreePath that describes the path to the wanted child.
+     */
+    template<typename Node, typename TreePath>
+    using ChildForTreePath = typename impl::_ChildForTreePath<Node,TreePath>::type;
+
+
+#ifndef DOXYGEN
+
+    namespace impl {
+
+      // By default, types are not flat indices
+      template<typename T>
+      struct _is_flat_index
+      {
+        using type = std::false_type;
+      };
+
+      // But std::size_t is
+      template<>
+      struct _is_flat_index<std::size_t>
+      {
+        using type = std::true_type;
+      };
+
+      // And so is any index_constant
+      template<std::size_t i>
+      struct _is_flat_index<index_constant<i>>
+      {
+        using type = std::true_type;
+      };
+
+    }
+
+#endif // DOXYGEN
+
+    //! Type trait that determines whether T is a flat index in the context of child extraction.
+    /*
+     * This type trait can be used to check whether T is a flat index (i.e. either `std::size_t`
+     * or `index_constant`). The type trait normalizes T before doing the check, so it will also
+     * work correctly for references and cv-qualified types.
+     */
+    template<typename T>
+    using is_flat_index = typename impl::_is_flat_index<typename std::decay<T>::type>::type;
 
 
     //! \} group TypeTree
