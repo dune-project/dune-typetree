645e6928e10d3418f405a731c2c405f2c6f8df70
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-09-21 14:59:53 +0200

    Add check if type represents a tree path
    Signed-off-by: Carsten Gräser <graeser@mi.fu-berlin.de>

--- dune/typetree/typetraits.hh
+++ dune/typetree/typetraits.hh
@@ -7,6 +7,8 @@
 #include <type_traits>
 #include <dune/common/typetraits.hh>
 
+#include <dune/typetree/treepath.hh>
+
 namespace Dune {
 
   // Provide some more C++11 TMP helpers.
@@ -168,6 +170,71 @@ namespace Dune {
         lazy_identity<F>
         >::type::type type;
     };
+
+    namespace impl {
+
+      // Check if type is a or is derived from one of the tree path types
+
+      // Default overload for types not representing a tree path
+      constexpr auto isTreePath(void*)
+        -> std::false_type
+      {
+        return std::false_type();
+      }
+
+      // Overload for DynamicTreePath
+      constexpr auto isTreePath(const DynamicTreePath*)
+        -> std::true_type
+      {
+        return std::true_type();
+      }
+
+      // Overload for instances of TreePath<...>
+      template<std::size_t... i>
+      constexpr auto isTreePath(const TreePath<i...>*)
+        -> std::true_type
+      {
+        return std::true_type();
+      }
+
+      // Overload for instances of HybridTreePath<...>
+      template<class... I>
+      constexpr auto isTreePath(const HybridTreePath<I...>*)
+        -> std::true_type
+      {
+        return std::true_type();
+      }
+
+    }
+
+    /**
+     * \brief Check if type represents a tree path
+     *
+     * If T is a or derived from one of the tree path types this
+     * struct derives from std::true_type, otherwise from std::false_type.
+     * Hence the result of the check is available via ::value, cast to bool,
+     * or operator().
+     *
+     * \tparam T Check if this type represents a tree path
+     */
+    template<class T>
+    struct IsTreePath :
+      public decltype(impl::isTreePath((typename std::decay<T>::type*)(nullptr)))
+    {};
+
+    /**
+     * \brief Check if given object represents a tree path
+     *
+     * \tparam T Check if this type represents a tree path
+     * \returns std::true_type if argument is a tree path and std::false_type if not
+     */
+    template<class T>
+    constexpr auto isTreePath(const T&)
+      -> IsTreePath<T>
+    {
+      return IsTreePath<T>();
+    }
+
 
   } // end namespace TypeTree
 } // end namespace Dune
