76e28bf7e5ab2ccb20369af0dfed4adfc0c00bf9
Christian Engwer <christi@mathe-macht-spass.de>
2020-05-19 19:26:08 +0000

    [!71] Make TreePath<i...> an alias for HybridTreePath<index_constant<i>...>
    Merge branch 'feature/merge-treepath-classes' into 'master'
    
    ref:staging/dune-typetree\> This removed the explicit implementation of
    TreePath<i...> and makes it an alias for HybridTreePath<index_constant<i>...>.
    While it is intended that TreePath is completely removed, there might be
    incompatibility issues if downstream modules switch to HybridTreePath but
    their users still use TreePath. Such issues should be avoided by this patch.
    
    See merge request [staging/dune-typetree!71]
    
      [staging/dune-typetree!71]: gitlab.dune-project.org/staging/dune-typetree/merge_requests/71
    
    
    (cherry picked from commit 420fc82d3c0dc0ef98c501b3de1b319998ddb8d3)
    
    6aa09b51 Make TreePath<i...> an alias for HybridTreePath<index_constant<i>...>

--- dune/typetree/treepath.hh
+++ dune/typetree/treepath.hh
@@ -30,112 +30,29 @@ namespace Dune {
       enum Type { fullyStatic, dynamic };
     }
 
-    template<std::size_t... i>
-    struct TreePath {
-
-      constexpr TreePath() = default;
-      constexpr TreePath(const TreePath&) = default;
-      constexpr TreePath(TreePath&&) = default;
-
-      typedef TreePath ViewType;
-      TreePath view() { return *this; }
-      TreePath mutablePath() { return *this; }
-    };
-
     template<typename>
     struct TreePathSize;
-
-    template<std::size_t... i>
-    struct TreePathSize<TreePath<i...> >
-      : public index_constant<sizeof...(i)>
-    {};
-
-     //! Returns the size (number of components) of the given `TreePath`.
-    template<std::size_t... i>
-    constexpr std::size_t treePathSize(const TreePath<i...>&)
-    {
-      return sizeof...(i);
-    }
 
     template<typename,std::size_t>
     struct TreePathPushBack;
 
-    template<std::size_t k, std::size_t... i>
-    struct TreePathPushBack<TreePath<i...>,k>
-    {
-      typedef TreePath<i...,k> type;
-    };
-
     template<typename,std::size_t>
     struct TreePathPushFront;
-
-    template<std::size_t k, std::size_t... i>
-    struct TreePathPushFront<TreePath<i...>,k>
-    {
-      typedef TreePath<k,i...> type;
-    };
 
     template<typename>
     struct TreePathBack;
 
-    // There is only a single element, so return that...
-    template<std::size_t k>
-    struct TreePathBack<TreePath<k> >
-      : public index_constant<k>
-    {};
-
-    // We need to explicitly provide two elements here, as
-    // the template argument pack would match the empty list
-    // and create a conflict with the single-element specialization.
-    // Here, we just shave off the first element and recursively
-    // instantiate ourselves.
-    template<std::size_t j, std::size_t k, std::size_t... l>
-    struct TreePathBack<TreePath<j,k,l...> >
-      : public TreePathBack<TreePath<k,l...> >
-    {};
-
     template<typename>
     struct TreePathFront;
-
-    template<std::size_t k, std::size_t... i>
-    struct TreePathFront<TreePath<k,i...> >
-      : public index_constant<k>
-    {};
 
     template<typename, std::size_t...>
     struct TreePathPopBack;
 
-    template<std::size_t k, std::size_t... i>
-    struct TreePathPopBack<TreePath<k>,i...>
-    {
-      typedef TreePath<i...> type;
-    };
-
-    template<std::size_t j,
-             std::size_t k,
-             std::size_t... l,
-             std::size_t... i>
-    struct TreePathPopBack<TreePath<j,k,l...>,i...>
-      : public TreePathPopBack<TreePath<k,l...>,i...,j>
-    {};
-
     template<typename>
     struct TreePathPopFront;
 
-    template<std::size_t k, std::size_t... i>
-    struct TreePathPopFront<TreePath<k,i...> >
-    {
-      typedef TreePath<i...> type;
-    };
-
     template<typename, typename>
     struct TreePathConcat;
-
-    template<std::size_t... i, std::size_t... k>
-    struct TreePathConcat<TreePath<i...>,TreePath<k...> >
-    {
-      typedef TreePath<i...,k...> type;
-    };
 
     template<std::size_t... i>
     void print_tree_path(std::ostream& os)
@@ -147,16 +64,6 @@ namespace Dune {
       os << k << " ";
       print_tree_path<i...>(os);
     }
-
-    template<std::size_t... i>
-    std::ostream& operator<<(std::ostream& os, const TreePath<i...>& tp)
-    {
-      os << "TreePath< ";
-      print_tree_path<i...>(os);
-      os << ">";
-      return os;
-    }
-
 
     //! A hybrid version of TreePath that supports both compile time and run time indices.
     /**
@@ -521,6 +428,9 @@ namespace Dune {
       os << ">";
       return os;
     }
+
+    template<std::size_t... i>
+    using TreePath = HybridTreePath<Dune::index_constant<i>...>;
 
     //! \} group TypeTree
 
