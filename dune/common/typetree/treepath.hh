f3cda4fe4afa30487cadc04d44d26e15ad035f05
Carsten Gräser <graeser@math.fau.de>
2025-11-28 13:26:55 +0100

    Unify namespace formating
    Use `namespace Dune::TypeTree` consistently. That's really just
    a tiny change. But when introducing this newly in dune-common,
    we can take the chance to introduce this in a clean form.

--- dune/common/typetree/treepath.hh
+++ dune/common/typetree/treepath.hh
@@ -17,83 +17,81 @@
 #include <dune/common/hybridmultiindex.hh>
 
 
-namespace Dune {
-  namespace TypeTree {
+namespace Dune::TypeTree {
 
-    //! \addtogroup TreePath
-    //! \ingroup TypeTree
-    //! \{
+  //! \addtogroup TreePath
+  //! \ingroup TypeTree
+  //! \{
 
-    //! A type for representing tree paths that supports both compile time and run time indices.
-    /**
-     * A `TreePath` supports storing a combination of run time and compile time indices.
-     * This makes it possible to store the tree path to a tree node inside the tree node itself,
-     * even if the path contains one or more `PowerNode`s, where each child must have exactly the
-     * same type. At the same time, as much information as possible is kept accessible at compile
-     * time, allowing for more efficient algorithms.
-     *
-     * \note Internally all indices are stored as std::size_t or
-     * std::integral_constant<std::size_t,v>. The latter is the same
-     * as Dune::index_constant<v>. If indices of other integral
-     * or std::integral_constant types are passed as arguments,
-     * they are converted.
-     *
-     * \note This is an alias for HybridMultiIndex
-     */
-    template<typename... T>
-    using TreePath = Dune::HybridMultiIndex<T...>;
+  //! A type for representing tree paths that supports both compile time and run time indices.
+  /**
+   * A `TreePath` supports storing a combination of run time and compile time indices.
+   * This makes it possible to store the tree path to a tree node inside the tree node itself,
+   * even if the path contains one or more `PowerNode`s, where each child must have exactly the
+   * same type. At the same time, as much information as possible is kept accessible at compile
+   * time, allowing for more efficient algorithms.
+   *
+   * \note Internally all indices are stored as std::size_t or
+   * std::integral_constant<std::size_t,v>. The latter is the same
+   * as Dune::index_constant<v>. If indices of other integral
+   * or std::integral_constant types are passed as arguments,
+   * they are converted.
+   *
+   * \note This is an alias for HybridMultiIndex
+   */
+  template<typename... T>
+  using TreePath = Dune::HybridMultiIndex<T...>;
 
-    //! Constructs a new `TreePath` from the given indices.
-    /**
-     * This function returns a new `TreePath` with the given index values. It exists
-     * mainly to avoid having to manually specify the exact type of the new object.
-     *
-     * It further ensures that the basic number type is std::size_t
-     * and casts any indices accordingly.
-     */
-    template<typename... T>
-    requires (((std::is_integral_v<T> or Dune::IsIntegralConstant<T>::value) && ...))
-    [[nodiscard]] constexpr auto treePath(const T&... t)
-    {
-      return HybridMultiIndex(t...);
-    }
+  //! Constructs a new `TreePath` from the given indices.
+  /**
+   * This function returns a new `TreePath` with the given index values. It exists
+   * mainly to avoid having to manually specify the exact type of the new object.
+   *
+   * It further ensures that the basic number type is std::size_t
+   * and casts any indices accordingly.
+   */
+  template<typename... T>
+  requires (((std::is_integral_v<T> or Dune::IsIntegralConstant<T>::value) && ...))
+  [[nodiscard]] constexpr auto treePath(const T&... t)
+  {
+    return HybridMultiIndex(t...);
+  }
 
-    // Pull in the free utility function for HybridMultiIndex/TreePath
-    // We cannot add forwarding functions of the same name here, since this
-    // leads to ambiguous overloads.
-    // Unfortunately doxygen ignores documentation for using statements.
-    using Dune::back;
-    using Dune::front;
-    using Dune::push_back;
-    using Dune::push_front;
-    using Dune::accumulate_back;
-    using Dune::accumulate_front;
-    using Dune::join;
-    using Dune::reverse;
-    using Dune::pop_front;
-    using Dune::pop_back;
+  // Pull in the free utility function for HybridMultiIndex/TreePath
+  // We cannot add forwarding functions of the same name here, since this
+  // leads to ambiguous overloads.
+  // Unfortunately doxygen ignores documentation for using statements.
+  using Dune::back;
+  using Dune::front;
+  using Dune::push_back;
+  using Dune::push_front;
+  using Dune::accumulate_back;
+  using Dune::accumulate_front;
+  using Dune::join;
+  using Dune::reverse;
+  using Dune::pop_front;
+  using Dune::pop_back;
 
-    inline namespace Literals {
+  inline namespace Literals {
 
-    //! Literal to create treepath
-    /**
-     * Example:
-     * `2_tp -> TreePath<index_constant<2>>`
-     **/
-    template <char... digits>
-    constexpr auto operator""_tp()
-    {
-      using namespace Dune::Indices::Literals;
-      return treePath(operator""_ic<digits...>());
-    }
+  //! Literal to create treepath
+  /**
+   * Example:
+   * `2_tp -> TreePath<index_constant<2>>`
+   **/
+  template <char... digits>
+  constexpr auto operator""_tp()
+  {
+    using namespace Dune::Indices::Literals;
+    return treePath(operator""_ic<digits...>());
+  }
 
-    } // end namespace Literals
+  } // end namespace Literals
 
 
-    //! \} group TypeTree
+  //! \} group TypeTree
 
-  } // namespace TypeTree
-} //namespace Dune
+} //namespace Dune::TypeTree
 
 
 
